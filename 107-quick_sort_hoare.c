f8b58792577c2ec8f96c49b6991cc11af53c221d
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-02-19 19:51:13 +0200

    try 107

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,97 @@
+#include "sort.h"
+
+/**
+ * partition_array - function divides an array into two parts
+ * for the quicksort algorithm.
+ *
+ * @array: Pointer to the array to be divided.
+ * @left_index: Index of the left boundary of the division.
+ * @right_index: Index of the right boundary of the division.
+ * @array_size: Size of the array.
+ *
+ * Return: The index of the pivot element after division.
+ */
+int partition_array(int *array, int left_index,
+					int right_index, size_t array_size)
+{
+	int temporary, pivot_index;
+	int iterator;
+
+	pivot_index = left_index - 1;
+
+	/* Iterate through the division */
+	for (iterator = left_index; iterator < right_index; iterator++)
+	{
+		/* If the current element is less than the pivot */
+		if (array[iterator] < array[right_index])
+		{
+			pivot_index++;
+			if (pivot_index != iterator)
+			{
+				/* Swap elements */
+				temporary = array[pivot_index];
+				array[pivot_index] = array[iterator];
+				array[iterator] = temporary;
+				/* Print the array after each swap */
+				print_array(array, array_size);
+			}
+		}
+	}
+
+	/* Swap the pivot element to its correct position */
+	if (array[right_index] < array[pivot_index + 1])
+	{
+		temporary = array[pivot_index + 1];
+		array[pivot_index + 1] = array[right_index];
+		array[right_index] = temporary;
+		/* Print the array after each swap */
+		print_array(array, array_size);
+	}
+
+	return (pivot_index + 1);
+}
+
+/**
+ * quick_sort_recursive - function recursively applies
+ * the quicksort algorithm to sort an array of integers.
+ *
+ * @array: Pointer to the array to be sorted.
+ * @left_index: Index of the left boundary of the partition.
+ * @right_index: Index of the right boundary of the partition.
+ * @array_size: Size of the array.
+ */
+void quick_sort_recursive(int *array, int left_index,
+						  int right_index, size_t array_size)
+{
+	int pivot_index;
+
+	/* If the left boundary is less than the right boundary */
+	if (left_index < right_index)
+	{
+		/* Partition the array and obtain the pivot index */
+		pivot_index = partition_array(array, left_index, right_index, array_size);
+
+		/* Apply quicksort recursively to the left partition */
+		quick_sort_recursive(array, left_index, pivot_index - 1, array_size);
+
+		/* Apply quicksort recursively to the right partition */
+		quick_sort_recursive(array, pivot_index + 1, right_index, array_size);
+	}
+}
+
+/**
+ * quick_sort_hoare - function sorts an array of integers
+ * using the Quick sort algorithm (Hoare partition scheme).
+ *
+ * @array: Pointer to the array to be sorted.
+ * @size:  The number of elements in the array.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	/* Check if the array contains less than two elements */
+	if (size < 2)
+		return;
+
+	/* Perform quick sort using the Hoare partition scheme */
+	quick_sort_recursive(array, 0, (int)size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -76,7 +76,8 @@ void bitonic_sort_recursion(int *array, int leftIndex,
 													int rightIndex, int direction, size_t size);
 void bitonic_sort(int *array, size_t size);
 
-
+/* Quick Sort - Hoare Partition scheme functions */
+void quick_sort_hoare(int *array, size_t size);
 
 
 /* Main functions */
